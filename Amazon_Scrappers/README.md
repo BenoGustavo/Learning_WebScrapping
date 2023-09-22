9b2147dc6fddec7426419fc25b9d28da5571b6ec
BenoGustavo <gustavoleandrogorges@gmail.com>
2023-09-22 12:11:41 -0300

    readme + amazon price scrapper

---
+++ Amazon_Scrappers/README.md
@@ -0,0 +1,42 @@
+# Amazon Price Scrappers
+
+# Libs:
+
+-   Requests
+-   BeautifulSoap4
+-   XlsxWriter
+
+> Creating a scrapper using Python🐍
+
+## 💻 Pre-Requisites
+
+I tested and programed this calculator in Linux (Ubuntu), so i don't know if it will work in other systems.
+
+-   [✓] Linux 🐧
+-   [✓] Windows 🪟
+-   [Don't have one to test] Mac 🍎
+
+## 🚀 Installation
+
+To install the project do this, i recommend using a env:
+
+Linux:
+
+```
+python3 -m venv .env
+source .env/bin/activate
+
+pip install -r requirements.txt
+
+python3 main.py
+```
+
+Windows:
+
+```
+???
+```
+
+## 😄 About me.
+
+I'm learning english, so if i spell something wrong it's part of the process, thanks.
---
+++ Amazon_Scrappers/main.py
@@ -0,0 +1,55 @@
+import requests
+import xlsxwriter
+import datetime
+from bs4 import BeautifulSoup
+
+URL = "https://www.amazon.com.br/gp/bestsellers/electronics/ref=zg_bs_electronics_sm"
+
+def getProductName(request):
+    html = BeautifulSoup(request.text, "html.parser")
+
+    #Getting product name
+    productsNames = html.find_all("div",class_ = "_cDEzb_p13n-sc-css-line-clamp-3_g3dy1")
+
+    #Formating product name
+    formatedProductsNames = []
+    for product in productsNames:
+        formatedProductsName = product.text
+        formatedProductsNames.append(formatedProductsName)
+
+    return formatedProductsNames
+
+def getProductPrices(request):
+    #Getting product prices
+    html = BeautifulSoup(request.text, "html.parser")
+
+    productsPrices = html.find_all('span', class_="p13n-sc-price")
+
+    formatedProductPrices = []
+    for product in productsPrices:
+        formatedProductPrices.append(product.text)
+        
+    return formatedProductPrices
+
+if __name__ == "__main__":
+    request = requests.get(URL)
+    productsName = getProductName(request)
+    productPrice = getProductPrices(request)
+
+    productPricesAndNames = list(zip(productsName,productPrice))
+    
+    workbook = xlsxwriter.Workbook('BestEletronicPricesAmazon.xlsx')
+    worksheet = workbook.add_worksheet()
+    worksheet.write('A1', 'Date')
+    worksheet.write('B1', 'ProductName')
+    worksheet.write('C1', 'ProductPrice')
+    row = 3
+
+    for product in productPricesAndNames:
+        time = current_datetime = datetime.datetime.now().strftime("%Y-%m-%d %H:%M")
+        worksheet.write('A' + str(row), time)
+        worksheet.write('B' + str(row), product[0])
+        worksheet.write('C' + str(row), product[1])
+        row += 1
+
+    workbook.close()
