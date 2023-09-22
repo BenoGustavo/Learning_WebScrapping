3b4d77e32cc50b9d131c26efb2cdfbdcf16848b7
BenoGustavo <gustavoleandrogorges@gmail.com>
2023-09-22 12:44:21 -0300

    adding the scond page to the scrap, the name and values don't match

--- Amazon_Scrappers/main.py
+++ Amazon_Scrappers/main.py
@@ -3,41 +3,52 @@ import xlsxwriter
 import datetime
 from bs4 import BeautifulSoup
 
-URL = "https://www.amazon.com.br/gp/bestsellers/electronics/ref=zg_bs_electronics_sm"
+PAGE1_URL = "https://www.amazon.com.br/gp/bestsellers/electronics/ref=zg_bs_electronics_sm"
+PAGE2_URL = "https://www.amazon.com.br/gp/bestsellers/electronics/ref=zg_bs_pg_2_electronics?ie=UTF8&pg=2"
 
-
-def getProductName(request):
-    html = BeautifulSoup(request.text, "html.parser")
+def getProductName(requestPage1,requestPage2):
+    html = BeautifulSoup(requestPage1.text, "html.parser")
+    htmlPage2 = BeautifulSoup(requestPage2.text, "html.parser")
 
     # Getting product name
     productsNames = html.find_all("div", class_="_cDEzb_p13n-sc-css-line-clamp-3_g3dy1")
+    productsNamesPage2 = htmlPage2.find_all("div", class_="_cDEzb_p13n-sc-css-line-clamp-3_g3dy1")
 
     # Formating product name
     formatedProductsNames = []
     for product in productsNames:
-        formatedProductsName = product.text
-        formatedProductsNames.append(formatedProductsName)
+        formatedProductsNames.append(product.text)
+
+    for product in productsNamesPage2:
+        formatedProductsNames.append(product.text)
 
     return formatedProductsNames
 
 
-def getProductPrices(request):
+def getProductPrices(requestPage1,requestPage2):
     # Getting product prices
-    html = BeautifulSoup(request.text, "html.parser")
+    html = BeautifulSoup(requestPage1.text, "html.parser")
+    htmlPage2 = BeautifulSoup(requestPage2.text, "html.parser")
 
     productsPrices = html.find_all("span", class_="p13n-sc-price")
+    productsPrices2 = htmlPage2.find_all("span", class_="p13n-sc-price")
 
     formatedProductPrices = []
     for product in productsPrices:
+        formatedProductPrices.append(product.text)
+
+    for product in productsPrices2:
         formatedProductPrices.append(product.text)
 
     return formatedProductPrices
 
 
 if __name__ == "__main__":
-    request = requests.get(URL)
-    productsName = getProductName(request)
-    productPrice = getProductPrices(request)
+    request_page1 = requests.get(PAGE1_URL)
+    request_page2 = requests.get(PAGE2_URL)
+
+    productsName = getProductName(request_page1,request_page2)
+    productPrice = getProductPrices(request_page1,request_page2)
 
     productPricesAndNames = list(zip(productsName, productPrice))
 
@@ -56,3 +67,5 @@ if __name__ == "__main__":
         row += 1
 
     workbook.close()
+
+    print("Scrap Concluido")
